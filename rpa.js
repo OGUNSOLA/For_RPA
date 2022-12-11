b203cd510ccbeac148b277d1a9abdddc441f5cb7
OLUBUKOLA OGUNSOLA <olubukola@me.com>
2022-12-11 08:04:47 -0500

    added project files

---
+++ rpa.js
@@ -0,0 +1,62 @@
+/** @format */
+
+const add = document.getElementsByClassName("add");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const address = document.getElementById("address");
+const city = document.getElementById("city");
+const province = document.getElementById("province");
+const gender = document.getElementById("gender");
+const homePhone = document.getElementById("home");
+const workPhone = document.getElementById("work");
+const personalEmail = document.getElementById("personal");
+const workEmail = document.getElementById("workEmail");
+const table = document.getElementById("customerData");
+let sn = 1;
+
+document.addEventListener("click", (e) => {
+  let customerID = Math.floor(Math.random() * 1000000 + 1);
+  const newRow = document.createElement("tr");
+  const snTd = document.createElement("td");
+  const fnTd = document.createElement("td");
+  const lnTd = document.createElement("td");
+  const genderTd = document.createElement("td");
+  const addTd = document.createElement("td");
+  const cityTd = document.createElement("td");
+  const provinceTd = document.createElement("td");
+  const hmphoneTd = document.createElement("td");
+  const wkphoneTd = document.createElement("td");
+  const pEmailTd = document.createElement("td");
+  const wEmailTd = document.createElement("td");
+  const cusIDTd = document.createElement("td");
+  if (e.target.className === "add") {
+    snTd.append(sn);
+    fnTd.append(firstName.value);
+    lnTd.append(lastName.value);
+    genderTd.append(gender.value);
+    addTd.append(address.value);
+    cityTd.append(city.value);
+    provinceTd.append(province.value);
+    hmphoneTd.append(homePhone.value);
+    wkphoneTd.append(workPhone.value);
+    pEmailTd.append(personalEmail.value);
+    wEmailTd.append(workEmail.value);
+    cusIDTd.append(customerID);
+    newRow.append(
+      snTd,
+      fnTd,
+      lnTd,
+      genderTd,
+      addTd,
+      cityTd,
+      provinceTd,
+      hmphoneTd,
+      wkphoneTd,
+      pEmailTd,
+      wEmailTd,
+      cusIDTd
+    );
+    sn += 1;
+    table.append(newRow);
+  }
+});
